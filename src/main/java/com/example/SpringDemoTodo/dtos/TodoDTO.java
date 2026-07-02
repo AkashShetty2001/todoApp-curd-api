3c3696b6d2c99428bedf3d54aad09bced73971f5
AkashShetty2001 <akashkd9911@gmail.com>
2026-07-02 17:58:34 +0530

    added dto layer for request body objects

---
+++ src/main/java/com/example/SpringDemoTodo/dtos/TodoDTO.java
@@ -0,0 +1,10 @@
+package com.example.SpringDemoTodo.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TodoDTO {
+    private String content;
+}
